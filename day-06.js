2f741674e750122b611c920efc0dfd72e72589e3
Arjan Kruithof <arjan.kruithof@npo.nl>
2022-12-06 11:27:23 +0100

    Solved day 06

---
+++ day-06.js
@@ -0,0 +1,28 @@
+const startTime = new Date().getTime();
+const dayNumber = '06';
+
+// load the file
+const fs = require('fs');
+const path = require('path');
+const appData = fs.readFileSync(path.resolve(__dirname, `day-${dayNumber}.txt`), 'utf8');
+
+// get the solution
+function getSolution(length) {
+  let solution = '';
+
+  for (let i = 0; i < appData.length; i += 1) {
+    const current = appData.slice(i, i + length);
+    if ([...new Set(current)].length === length) {
+      solution = i + (length);
+      break;
+    }
+  }
+
+  return solution;
+}
+
+console.log(getSolution(4));
+console.log(getSolution(14));
+
+// show runtime for fun
+console.log('runtime', `${new Date().getTime() - startTime}ms`);
